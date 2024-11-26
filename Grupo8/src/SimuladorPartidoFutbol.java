61c780fef08213dba7ce86156f837db658624cec
Dostin89 <mazadosti@gmail.com>
2024-11-26 06:25:57 -0500

    Taller8

--- Grupo8/src/SimuladorPartidoFutbol.java
+++ Grupo8/src/SimuladorPartidoFutbol.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 public class SimuladorPartidoFutbol {
     public static void main(String[] args) {
         Random random = new Random();
@@ -55,3 +54,38 @@ public class SimuladorPartidoFutbol {
         }
     }
 }
+
+/***
+ * EQUIPO A
+NOMBRE      | POSICI�N  | EDAD  | GOLES |
+Jugador A1   | Posici�n1   | 34    | 2    |
+Jugador A2   | Posici�n2   | 19    | 2    |
+Jugador A3   | Posici�n3   | 34    | 1    |
+Jugador A4   | Posici�n4   | 29    | 2    |
+Jugador A5   | Posici�n5   | 35    | 2    |
+Jugador A6   | Posici�n6   | 25    | 1    |
+Jugador A7   | Posici�n7   | 37    | 0    |
+Jugador A8   | Posici�n8   | 40    | 1    |
+Jugador A9   | Posici�n9   | 22    | 0    |
+Jugador A10   | Posici�n10   | 26    | 1    |
+Jugador A11   | Posici�n11   | 22    | 1    |
+
+EQUIPO B
+NOMBRE      | POSICI�N  | EDAD  | GOLES |
+Jugador B1   | Posici�n1   | 39    | 0    |
+Jugador B2   | Posici�n2   | 26    | 2    |
+Jugador B3   | Posici�n3   | 25    | 2    |
+Jugador B4   | Posici�n4   | 37    | 0    |
+Jugador B5   | Posici�n5   | 37    | 2    |
+Jugador B6   | Posici�n6   | 23    | 1    |
+Jugador B7   | Posici�n7   | 23    | 0    |
+Jugador B8   | Posici�n8   | 33    | 2    |
+Jugador B9   | Posici�n9   | 26    | 1    |
+Jugador B10   | Posici�n10   | 19    | 2    |
+Jugador B11   | Posici�n11   | 25    | 0    |
+
+ESTAD�STICA DEL PARTIDO:
+Goles Equipo A: 12
+Goles Equipo B: 11
+Equipo ganador: Equipo A.
+ */
